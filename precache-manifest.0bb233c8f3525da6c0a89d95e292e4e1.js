a9da42638e7ceb4c5be183840a8071feff157f7d
GabrielDSantos <gabrielhenrique022@gmail.com>
2024-10-30 16:34:29 -0300

    Updates

---
+++ precache-manifest.0bb233c8f3525da6c0a89d95e292e4e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfda002c0b35f6b39ada1d3922df13fa",
+    "url": "/FENT-master/index.html"
+  },
+  {
+    "revision": "d4f4fde846283e0dfb31",
+    "url": "/FENT-master/static/css/main.97965c49.chunk.css"
+  },
+  {
+    "revision": "e951bb0050d4c3ea9db0",
+    "url": "/FENT-master/static/js/2.56390d5e.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/FENT-master/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "d4f4fde846283e0dfb31",
+    "url": "/FENT-master/static/js/main.488bc724.chunk.js"
+  },
+  {
+    "revision": "06f282f68661d1335bc1",
+    "url": "/FENT-master/static/js/runtime~main.a21c8023.js"
+  },
+  {
+    "revision": "452be62dd6d1ed2b7748689b184bdf44",
+    "url": "/FENT-master/static/media/logo-fent-2.452be62d.png"
+  }
+]);
